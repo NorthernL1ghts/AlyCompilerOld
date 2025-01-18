f068037a83825fa7178ee79f040b973942ed40d7
NorthernL1ghts <NorthernL1ghts@gmail.com>
2025-01-18 21:10:44 +0000

    Basic Entry Point + print_usage method

--- src/main.c
+++ src/main.c
@@ -1,3 +1,15 @@
-int main(void) {
-	return 420;
+#include <stdio.h>
+
+void print_usage(char** argv) {
+	printf("USAGE: %s <path_to_file_to_compile>\n", argv[0]);
+}
+
+int main(int argc, char** argv) {
+	if (argc < 2) {
+		print_usage(argv);
+		exit(0);
+	}
+
+
+	return 0;
 }
