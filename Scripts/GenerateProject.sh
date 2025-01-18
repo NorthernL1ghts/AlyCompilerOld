080d6573769fd27e44f9940d0106c57ecd97f15f
NorthernL1ghts <NorthernL1ghts@gmail.com>
2025-01-18 21:05:15 +0000

    Added Shell script to generate the sln for Linux and MacOS users

---
+++ Scripts/GenerateProject.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to the parent directory
+cd ..
+
+# Configure the project using CMake with the Visual Studio 2022 generator and x64 toolset
+cmake -G "Visual Studio 17 2022" -A x64 -B build -DCMAKE_BUILD_TYPE=Release
+
+# Build the project in Release mode
+cmake --build build --config Release
