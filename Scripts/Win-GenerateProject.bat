e58d0d0465214c39684872bbb138f5fce9b9e60b
NorthernL1ghts <NorthernL1ghts@gmail.com>
2025-03-26 21:59:31 +0000

    Added comments to Window's build script as we didn't have and it didn't match the Shell version!

--- Scripts/Win-GenerateProject.bat
+++ Scripts/Win-GenerateProject.bat
@@ -1,4 +1,10 @@
 @echo off
+
+@REM Navigate to the parent directory
 cd ..
+
+@REM Configure the project using CMake with the Visual Studio 2022 generator and x64 toolset
 cmake -G "Visual Studio 17 2022" -A x64 -B build -DCMAKE_BUILD_TYPE=Release
+
+@REM Build the project in Release mode
 cmake --build build --config Release
