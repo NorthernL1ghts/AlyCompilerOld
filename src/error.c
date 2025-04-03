6dcb3d72f5057d6706fa73c1e2acb20f293e5f4c
NorthernL1ghts <NorthernL1ghts@gmail.com>
2025-04-03 16:02:41 +0100

    # Restructure Codebase: Split main.c into Modular Components
    ## Changes
    - Extracted **Main Logic** into main.c
    - Separated **Parser** into parser.c/h
    - Isolated **File I/O** into ile_io.c/h
    - Organized **Environment Handling** into env.c/h
    - Dedicated **Error Management** to error.c/h
    
    ## Benefits
    - Improves maintainability and readability
    - Enhances modularity for easier future development
    - Simplifies debugging and testing

---
+++ src/error.c
@@ -0,0 +1,38 @@
+#include <error.h>
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdio.h>
+
+Error ok = { ERROR_NONE, NULL };
+
+void print_error(Error err) {
+	if (err.type == ERROR_NONE) { return; }
+	printf("ERROR: ");
+	assert(ERROR_MAX == 6);
+	switch (err.type) {
+	default:
+		printf("Unknown error type...");
+		break;
+	case ERROR_TODO:
+		printf("TODO (not implemented)");
+		break;
+	case ERROR_SYNTAX:
+		printf("Invalid Syntax");
+		break;
+	case ERROR_TYPE:
+		printf("Mismatched types");
+		break;
+	case ERROR_ARGUMENTS:
+		printf("Invalid arguments");
+		break;
+	case ERROR_GENERIC:
+		break;
+	case ERROR_NONE:
+		break;
+	}
+	putchar('\n');
+	if (err.msg) {
+		printf("     : %s\n", err.msg);
+	}
+}
