d35869a444a512bf3a904064806751b08ebb37a8
NorthernL1ghts <NorthernL1ghts@gmail.com>
2025-04-17 12:09:12 +0100

    Renamed 'build' folder to 'bld' to match how C looks in ROOT

--- Scripts/GenerateProject.sh
+++ Scripts/GenerateProject.sh
@@ -4,7 +4,7 @@
 cd ..
 
 # Configure the project using CMake with the Visual Studio 2022 generator and x64 toolset
-cmake -G "Visual Studio 17 2022" -A x64 -B build -DCMAKE_BUILD_TYPE=Debug
+cmake -G "Visual Studio 17 2022" -A x64 -B bld -DCMAKE_BUILD_TYPE=Debug
 
 # Build the project in Debug mode
-cmake --build build --config Debug
+cmake --build bld --config Debug
