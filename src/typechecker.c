bd1ea1f1dffea2c8067fb2d9d45f727519d5ecec
NorthernL1ghts <34502005+NorthernL1ghts@users.noreply.github.com>
2025-05-06 17:42:01 +0100

    Type Checking Update: We are able to now type check function call argument expressions

--- src/typechecker.c
+++ src/typechecker.c
@@ -19,11 +19,33 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+int expression_return_type(ParsingContext* context, Node* expression) {
+    Node* result;
+    switch (expression->type) {
+    default:
+        return expression->type;
+    case NODE_TYPE_FUNCTION_CALL:
+        while (context) {
+            if (environment_get(*context->functions, expression->children, result)) {
+                break;
+            }
+            context = context->parent;
+        }
+        // RESULT contains a function node.
+        print_node(result, 0);
+        break;
+    }
+}
 
 Error typecheck_expression(ParsingContext* context, Node* expression) {
+    Error err = ok;
     Node* value = node_allocate();
     Node* tmpnode = node_allocate();
     Node* iterator = node_allocate();
+    Node* result = node_allocate();
+    int type = NODE_TYPE_NONE;
     switch (expression->type) {
     default:
         break;
@@ -40,23 +62,36 @@ Error typecheck_expression(ParsingContext* context, Node* expression) {
         iterator = expression->children->next_child->children;
         tmpnode = value->children->children;
         while (iterator && tmpnode) {
-            //print_node(tmpnode->children->next_child, 0);
+            err = parse_get_type(context, tmpnode->children->next_child, result);
+            if (err.type) { break; }
+            if (expression_return_type(context, iterator) != result->type) {
+                printf("Function:%s\n", expression->children->value.symbol);
+                ERROR_PREP(err, ERROR_TYPE, "Argument type does not match declared parameter type");
+                break;
+            }
+            else {
+                printf("TYPES MATCH!\n");
+            }
             iterator = iterator->next_child;
             tmpnode = tmpnode->next_child;
         }
         if (tmpnode != NULL) {
             printf("Function:%s\n", expression->children->value.symbol);
-            ERROR_CREATE(err, ERROR_ARGUMENTS, "Not enough arguments passed to function!");
-            return err;
+            ERROR_PREP(err, ERROR_ARGUMENTS, "Not enough arguments passed to function!");
+            break;
         }
         if (iterator != NULL) {
             printf("Function:%s\n", expression->children->value.symbol);
             ERROR_CREATE(err, ERROR_ARGUMENTS, "Too many arguments passed to function!");
-            return err;
+            break;
         }
         break;
     }
-    return ok;
+    free(result);
+    free(value);
+    free(tmpnode);
+    free(iterator);
+    return err;
 }
 
 Error typecheck_program(ParsingContext* context, Node* program) {
